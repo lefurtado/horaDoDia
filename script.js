5379294b4ff0254eddb01e2a9c1e4c079c2e1625
Leandro Furtado <le_furtado@outlook.com.br>
2022-06-22 12:34:19 -0300

    adicionado alerts conforme a hora do dia

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ function carregar() {
 
     msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}`
     if (hora >= 5 && hora < 12) {
-        //Bom dia
+        alert(`Bom dia!`)
         img.src = "./img/fotomanha.png";
         document.body.style.background = '#ffe353';
     } else if (hora >= 12 && hora < 18) {
-        //Boa tarde
+        alert(`Boa tarde!`)
         img.src = "./img/fototarde.png";
         document.body.style.background = '#e5cba3';
     } else {
-        //Boa noite
+        alert(`Boa noite!`)
         img.src = "./img/fotonoite.png";
         document.body.style.background = '#193f58';
     }
